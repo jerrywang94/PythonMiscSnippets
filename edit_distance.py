b3053349f29ec6e87356f5b9abc1f43dc9277e18
Jerry <wyanze24@hotmail.com>
2024-05-30 20:12:24 -0700

    KMP string search, longest common subsequence

--- edit_distance.py
+++ edit_distance.py
@@ -25,3 +25,32 @@ def edit_distance(a: Sequence, b: Sequence) -> int:
         m0 = m1[::]
 
     return m1[-1]
+
+
+# Returns one instance of the longest common subsequence. Can be easily extended to return all longest subsequences.
+# This is the non-recursive 2-line method to reduce mem usage.
+def longest_common_subsequence(a: str, b: str) -> str:
+    a_len = len(a)
+    b_len = len(b)
+
+    if a_len == 0 or b_len == 0:
+        return ""
+
+    # Reduces memory footprint to the shorter of the two sequences
+    if a_len < b_len:
+        return longest_common_subsequence(b, a)
+
+    m0 = [""] * (b_len+1)
+    m1 = [""] * (b_len+1)
+
+    for i in range(1, a_len+1):
+        m1[0] = ""
+
+        for j in range(1, b_len+1):
+            extended = m0[j-1] + a[i-1] if a[i-1] == b[j-1] else ""
+            candidates = [(len(m1[j-1]), m1[j-1]), (len(extended), extended), (len(m0[j]), m0[j])]
+            m1[j] = max(candidates)[1]
+
+        m0 = m1[::]
+
+    return m1[-1]
---
+++ string_search.py
@@ -0,0 +1,30 @@
+# KMP string search
+def string_search(text: str, s: str) -> list[int]:
+    res = []
+    backtrack = -1
+
+    p = _build_prefix_table(s)
+ 
+    for i in range(len(text)):
+        while backtrack >= 0 and text[i] != s[backtrack + 1]:
+            backtrack = p[backtrack]
+        if text[i] == s[backtrack + 1]:
+            backtrack += 1
+        print(backtrack)
+        if backtrack == len(s) - 1:
+            res.append(i - backtrack)
+            backtrack = p[backtrack]
+    return res
+
+
+def _build_prefix_table(s: str) -> list[int]:
+    len_s = len(s)
+    p = [-1] * len_s
+    backtrack = -1
+    for i in range(1, len_s):
+        while backtrack >= 0 and s[i] != s[backtrack + 1]:
+            backtrack = p[backtrack]
+        if s[i] == s[backtrack + 1]:
+            backtrack += 1
+        p[i] = backtrack
+    return p
